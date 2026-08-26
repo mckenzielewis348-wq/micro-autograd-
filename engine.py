7ac33348476037a45cb16674793f4e6aeb420b5f
Mckenzie Lewis <307806605+mckenzielewis348-wq@users.noreply.github.com>
2026-08-26 15:11:58 -0500

    Refactor Value class for improved functionality

--- engine.py
+++ engine.py
@@ -1,81 +1,58 @@
 class Value:
-    def __init__(self, data, _children=(), _op=''):
-        self.data = float(data)
+
+    def __init__(self, data, _children=(), _op=""):
+        self.data = data
         self.grad = 0.0
         self._backward = lambda: None
         self._prev = set(_children)
         self._op = _op
 
-    def __repr__(self):
-        return f"Value(data={self.data}, grad={self.grad})"
-
     def __add__(self, other):
         other = other if isinstance(other, Value) else Value(other)
-        out = Value(self.data + other.data, (self, other), '+')
+        out = Value(self.data + other.data, (self, other), "+")
 
         def _backward():
             self.grad += out.grad
             other.grad += out.grad
+
         out._backward = _backward
-
         return out
-
-    def __radd__(self, other):
-        return self + other
 
     def __mul__(self, other):
         other = other if isinstance(other, Value) else Value(other)
-        out = Value(self.data * other.data, (self, other), '*')
+        out = Value(self.data * other.data, (self, other), "*")
 
         def _backward():
             self.grad += other.data * out.grad
             other.grad += self.data * out.grad
+
         out._backward = _backward
-
-        return out
-
-    def __rmul__(self, other):
-        return self * other
-
-    def __pow__(self, other):
-        assert isinstance(other, (int, float)), "only supporting int/float powers for now"
-        out = Value(self.data**other, (self,), f'**{other}')
-
-        def _backward():
-            self.grad += (other * (self.data ** (other - 1))) * out.grad
-        out._backward = _backward
-
         return out
 
     def relu(self):
-        out = Value(0.0 if self.data < 0 else self.data, (self,), 'ReLU')
+        out = Value(max(0, self.data), (self,), "ReLU")
 
         def _backward():
             self.grad += (out.data > 0) * out.grad
-        out._backward = _backward
 
+        out._backward = _backward
         return out
 
-    def __neg__(self):
-        return self * -1
-
-    def __sub__(self, other):
-        return self + (-other)
-
-    def __rsub__(self, other):
-        return Value(other) + (-self)
-
     def backward(self):
+        # Build topological order
         topo = []
         visited = set()
+
         def build_topo(v):
             if v not in visited:
                 visited.add(v)
                 for child in v._prev:
                     build_topo(child)
                 topo.append(v)
+
         build_topo(self)
 
+        # Set base gradient and apply chain rule in reverse
         self.grad = 1.0
         for node in reversed(topo):
             node._backward()
