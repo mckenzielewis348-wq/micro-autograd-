8cc8e8363e7d2e31f3fe9694fe748c1a8d57cd44
Mckenzie Lewis <307806605+mckenzielewis348-wq@users.noreply.github.com>
2026-08-26 13:49:30 -0500

    Implement ReLU method in engine.py
    Add ReLU activation function to engine.py

--- engine.py
+++ engine.py
@@ -47,6 +47,15 @@ class Value:
 
         return out
 
+    def relu(self):
+        out = Value(0.0 if self.data < 0 else self.data, (self,), 'ReLU')
+
+        def _backward():
+            self.grad += (out.data > 0) * out.grad
+        out._backward = _backward
+
+        return out
+
     def __neg__(self):
         return self * -1
 
